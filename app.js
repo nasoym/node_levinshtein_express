3f50c10da19fed512b93b072df3c303abcda2e08
Sinan Goo <sinan.goo@hypoport.de>
2016-12-12 16:43:43 +0100

    add levinsthein algorithm

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Levenshtein = require('levenshtein')
+
 var app = express()
 
 app.get('/', function (req, res) {
@@ -20,10 +22,8 @@ app.post('/text', bodyParser.text({type:"*/*"}), function (req, res) {
   var body = req.body;
   var firstLine = body.split('\n')[0];
   var secondLine = body.split('\n')[1];
-  var response = "first: " + firstLine;
-  response += "\n";
-  response += "second: " + secondLine;
-  response += "\n";
+  var levenshtein = new Levenshtein(firstLine, secondLine);
+  var response = "distance: " + levenshtein.distance + " \n";
   res.send(response);
 })
 
