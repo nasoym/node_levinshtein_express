5d0be9ba7b667db9c641d6c937026904c3c7c4a2
Sinan Goo <sinan.goo@hypoport.de>
2016-12-12 16:55:02 +0100

    remove unused routes

--- app.js
+++ app.js
@@ -4,21 +4,7 @@ var Levenshtein = require('levenshtein')
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send("hello world\n");
-})
-
-app.post('/json', bodyParser.json(), function (req, res) {
-  console.log("body: ", req.body);
-  res.send("hello world\n");
-})
-
- app.post('/raw', bodyParser.raw(), function (req, res) {
-  console.log("body: " + req.body.toString());
-  res.send("hello world\n");
-})
-
-app.post('/text', bodyParser.text({type:"*/*"}), function (req, res) {
+app.post('/', bodyParser.text({type:"*/*"}), function (req, res) {
   var body = req.body;
   var firstLine = body.split('\n')[0];
   var secondLine = body.split('\n')[1];
