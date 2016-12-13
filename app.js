1562b7e8641ad7bdb60a3c2d3d95822c9705f71d
Sinan Goo <sinan.goo@hypoport.de>
2016-12-13 13:07:35 +0100

    add debug logging

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var Levenshtein = require('levenshtein')
 var app = express()
 
 app.post('/', bodyParser.text({type:"*/*"}), function (req, res) {
+  if (process.env.DEBUG=="1") {
+    console.error(req);
+  }
   var body = req.body;
   var firstLine = body.split('\n')[0];
   var secondLine = body.split('\n')[1];
   var levenshtein = new Levenshtein(firstLine, secondLine);
   var response = "distance: " + levenshtein.distance + " \n";
+  if (process.env.DEBUG=="1") {
+    console.error(response);
+  }
   res.send(response);
 })
 
